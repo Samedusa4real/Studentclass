09b5500591b6dcca3a203a0922a4a27cdda11bc3
Samedusaa <semed.musayev.2003@mail.ru>
2022-12-17 01:40:58 +0400

    committed new files

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("Zehmet olmasa adinizi, soyadinizi ve qrup nomrenizi daxil edin:");
+Console.Write("AD:"); string name1 = Console.ReadLine();
+Console.Clear();
+Console.Write("SOYAD:"); string surname1 = Console.ReadLine();
+Console.Clear();
+Console.Write("QRUP NOMRESI:"); string group1 = Console.ReadLine();
+Console.Clear();
+Student student1 = new Student(name1, surname1,group1);
+
+Console.WriteLine($"{group1} qrupunun telebesi {name1} {surname1} xos geldiniz, zehmet olmasa tehsil veziyyetinizi secerdiniz:");
+
+chooseGraduation();
+
+string choose = Console.ReadLine();
+Console.Clear();
+while (choose != default)
+{
+    Console.Clear();
+    switch (choose)
+    {
+        case "1":
+            Console.WriteLine("Size gelecek heyatinizda ugurlar!");
+            return;
+        case "2":
+            Console.WriteLine("Zehmet olmasa imtahandan aldiginiz bali qeyd ederdiniz:");
+            int point1 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Student newChance = new Student(point1);
+            return;
+        default:
+            Console.WriteLine("Zehmet olmasa duzgun secin!");
+            chooseGraduation();
+            choose = Console.ReadLine();
+            break;
+    }
+}
+
+
+
+
+
+
+
+static void chooseGraduation()
+{
+    Console.WriteLine("1.Mezun olmusam.");
+    Console.WriteLine("2.Hele de tehsil aliram.");
+}
---
+++ Student.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.ExceptionServices;
+
+internal class Student
+{
+    public string Name;
+    public string SurName;
+    public string Group;
+    public int Point;
+    public bool isGraduated;
+
+    public Student(string name, string surname,string group)
+    {
+        this.Name = name;
+        this.SurName= surname;
+        this.Group = group;
+    }
+
+    public Student(string name, string surname, string group, int point, bool isGraduated) : this(name, surname,group)
+    {
+        Point = point;
+        this.isGraduated = isGraduated;
+    }
+
+    public Student(bool isGraduated)
+    {
+        this.isGraduated = isGraduated;
+    }
+
+
+    public Student(int point)
+    {
+        this.Point = point;       
+            if (point < 101 && point > 0)
+            {
+                if (point > 80)
+                {
+                    Console.WriteLine("Tebrikler!!! Siz imtahanda yeniden istirak ede bilersiniz.");
+                }
+                else
+                {
+                    Console.WriteLine("Siz imtahanda yeniden istirak ede bilmersiniz!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Zehmet olmasa balinizi duzgun daxil edin!");
+            }
+    }
+}
